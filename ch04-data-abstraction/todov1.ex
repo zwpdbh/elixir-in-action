356a0da62f8d3889ddffbd2a69c61301fa2cf26e
unknown <hyperion_z@outlook.com>
2022-02-12 20:36:27 +0800

    >4.1.3

---
+++ ch04-data-abstraction/todov1.ex
@@ -0,0 +1,18 @@
+defmodule TodoList do
+  def new() do
+    %{}
+  end
+  
+  def add_entry(todo_list, date, title) do
+    Map.update(
+      todo_list,
+      date,
+      [title],
+      fn titles -> [title | titles] end
+    )
+  end
+  
+  def entries(todo_list, date) do
+    Map.get(todo_list, date, [])
+  end
+end
