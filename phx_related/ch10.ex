7a019d6f9b7b7b0c7e8661a3e07d4de61e82281e
zwpdbh <hyperion_z@outlook.com>
2022-03-27 18:51:12 +0800

    < no error, > Tracking presence on a channel

---
+++ phx_related/ch10.ex
@@ -0,0 +1,6 @@
+defmodule Ch10 do
+  def debug01() do
+    # When I click post to do video annotation, the iex console shows error:
+    # (KeyError) key :user_id not found in: %Rumbl.Accounts.User{...}
+  end
+end
