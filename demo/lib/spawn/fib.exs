7fb9f35e04f9bb3d464c34e35e27cb84cdab0bfa
Wei Zhao <zhaowei@microsoft.com>
2022-04-14 09:51:09 +0800

    <

--- demo/lib/spawn/fib.exs
+++ demo/lib/spawn/fib.exs
@@ -27,3 +27,16 @@ defmodule Scheduler do
     |> schedule_processes(to_calculate, [])
   end
 end
+
+
+
+to_process = List.duplicate(37, 20)
+Enum.each 1..10, fn num_processes ->
+  {time, result} = :timer.tc(Scheduler, :run, [num_processes, FibSolver, :fib, to_process])
+
+  if num_processes == 1 do
+    IO.puts inspect result
+    IO.puts "\n #   times (s)"
+  end
+  :io.format "~2B     ~.2f~n", [num_processes, time/100000.0]
+end
