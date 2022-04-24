89d24b23a4d6a9742417b8c0ac2d6563f03e01f1
Wei Zhao <zhaowei@microsoft.com>
2022-04-24 16:08:19 +0800

    >> scrape pages concurrently with more consumers

--- GenStage_examples/scraper/lib/page_producer.ex
+++ GenStage_examples/scraper/lib/page_producer.ex
@@ -13,9 +13,22 @@ defmodule PageProducer do
     {:producer, initial_state}
   end
 
+  # Got called when the producer receive demand from a consumer.
+  # It is useful when we want to respond to the consumer demand immediately and keep consumer busy.
   def handle_demand(demand, state) do
     Logger.info("Received demand for #{demand} pages")
     events = []
     {:noreply, events, state}
   end
+
+  def scrape_pages(pages) when is_list(pages) do
+    GenStage.cast(__MODULE__, {:pages, pages})
+  end
+
+  # Here, in handle_cast callback, we update the events needed to be processed as our pages.
+  def handle_cast({:pages, pages}, state) do
+    # {:noreply, [event], new_state}
+    {:noreply, pages, state}
+  end
+  
 end
