ac0c6ee41dad27c8839ce53c78eaaaf61686287a
zwpdbh <hyperion_z@outlook.com>
2022-06-05 21:03:22 +0800

    >> paip 4.5

--- paip/lib/ch04.ex
+++ paip/lib/ch04.ex
@@ -1,29 +1,89 @@
 defmodule GPS.Op do
   defstruct [
+    # represent the action 
     action: nil,
-    preconids: [],
+    # represent the precondition need to be met to take such action 
+    preconds: [],
+    # represent things needed to be added to global state after taking this action
     add_lst: [],
+    # represent things needed to be removed from global state after taking this action
     del_lst: []
   ]
 end
 
-defmodule GPS do
+defmodule GPSAgent do
+  use Agent
 
-  def gps() do
+  def start_link({state, ops}) do
+    Agent.start_link(fn -> {state, ops} end, name: __MODULE__)
+  end
+
+  def update_state_after_op(op) do
+    Agent.update(__MODULE__, fn {state, ops} ->
+      updated_state =
+        MapSet.difference(MapSet.new(state), MapSet.new(op.del_lst))
+        |> MapSet.union(MapSet.new(op.add_lst))
+        |> MapSet.to_list()
+
+      {updated_state, ops}
+    end)
+  end
+
+  def get_state do
+    Agent.get(__MODULE__, fn {state, _} -> state end)
+  end
+
+  def get_ops do
+    Agent.get(__MODULE__, fn {_, ops} -> ops end)
+  end
+end
+
+defmodule GPS do
+  def gps(state, goals, ops) do
+    GPSAgent.start_link({state, ops})
+
+    case Enum.all?(goals, fn x -> achieve(x) end) do
+      true ->
+        IO.puts("SOLVED")
+        "solved"
+
+      false ->
+        IO.puts("NOT SOLVED")
+        "not solved"
+    end
   end
 
   def appropriate?(goal, op = %GPS.Op{}) do
-    Common.member(goal, op.add_lst)
-  end
-  
-  def achieve(goal, state, ops) do
-    Common.member(goal, state) or Common.some(&GPS.apply_op/1, Common.find_all(goal, ops, &GPS.appropriate?/2))
+    # "An op is appropriate to a goal if it is in its add list."
+    op.add_lst
+    |> Enum.find(false, fn x -> goal == x end)
   end
 
-  def apply_op(op = %GPS.Op{}) do
-    case Common.every(&GPS.achieve/3, op.preconids) do
+  def achieve(goal) do
+    #  "A goal is achieved if it already holds, or if there is an appropriate op for it that is applicable."
+    case Enum.find(GPSAgent.get_state(), false, fn x -> goal == x end) do
+      false ->
+        GPSAgent.get_ops()
+        |> Enum.filter(fn x -> appropriate?(goal, x) end)
+        |> Enum.find(false, fn x -> applicable_op(x) end)
+
+      _ ->
+        # IO.puts("#{goal} is already achieved")
+        true
+    end
+  end
+
+  def applicable_op(op = %GPS.Op{}) do
+    # "Print a message and update *state* if op is applicable."
+    case Enum.all?(op.preconds, fn x -> achieve(x) end) do
       true ->
-        IO.puts "executing ${op.action}"
+        IO.puts("executing '#{op.action}'")
+        # How to update some global state
+        GPSAgent.update_state_after_op(op)
+        true
+
+      false ->
+        nil
     end
   end
 end
--- paip/test/ch04_test.exs
+++ paip/test/ch04_test.exs
@@ -1,9 +1,82 @@
 defmodule Ch04Test do
   use ExUnit.Case, async: true
-
+  setup do
+    {:ok,
+     school_ops: [
+        %GPS.Op{
+          action: "drive son to school",
+          preconds: ["son at home", "car works"],
+          add_lst: ["son at school"],
+          del_lst: ["son at home"]
+        },
+        %GPS.Op{
+          action: "show install battery",
+          preconds: ["car needs battery", "shop knows problem", "shop has money"],
+          add_lst: ["car works"]
+        },
+        %GPS.Op{
+          action: "tell shop problem",
+          preconds: ["in communication with shop"],
+          add_lst: ["shop knows problem"]
+        },
+        %GPS.Op{
+          action: "telephone shop",
+          preconds: ["know phone number"],
+          add_lst: ["in communication with shop"]
+        },
+        %GPS.Op{
+          action: "look up number",
+          preconds: ["have phone book"],
+          add_lst: ["know phone number"]
+        },
+        %GPS.Op{
+          action: "give shop money",
+          preconds: ["have money"],
+          add_lst: ["shop has money"],
+          del_lst: ["have money"]
+        }
+      ]
+    }
+  end
+  
   describe "part one" do
-    test "drive to school" do
-      IO.puts "TODO"
+    test "drive to school", %{school_ops: school_ops} do
+      current_state = [
+        "son at home",
+        "car needs battery",
+        "have money",
+        "have phone book"
+      ]
+      goals = [
+        "son at school"
+      ]
+
+      assert "solved" = GPS.gps(current_state, goals, school_ops)
     end
+
+    test "drive to school 02", %{school_ops: school_ops} do
+      current_state = [
+        "son at home",
+        "car needs battery",
+        "have money"
+      ]
+      goals = [
+        "son at school"
+      ]
+
+      assert "not solved" = GPS.gps(current_state, goals, school_ops)
+    end
+
+    test "drive to school 03", %{school_ops: school_ops} do
+      current_state = [
+        "son at school"
+      ]
+      goals = [
+        "son at school"
+      ]
+
+      assert "solved" = GPS.gps(current_state, goals, school_ops)
+    end
+    
   end
 end
