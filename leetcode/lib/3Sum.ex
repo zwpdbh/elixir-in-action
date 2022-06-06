f88da8a4cc4b7b3fec0f93b44cc63c71d75aaff1
Wei Zhao <zhaowei@microsoft.com>
2022-06-06 22:38:27 +0800

    <

--- leetcode/lib/3Sum.ex
+++ leetcode/lib/3Sum.ex
@@ -2,8 +2,6 @@ defmodule ThreeSum do
   # https://leetcode.com/problems/3sum/
   @spec three_sum(nums :: [integer]) :: [[integer]]
   def three_sum(nums) do
-    Result.start_link()
-    
     three_sum_aux(nums)
   end
 
@@ -15,35 +13,53 @@ defmodule ThreeSum do
     []
   end
 
+  def three_sum_aux([_, _]) do
+    []
+  end
+
   def three_sum_aux(nums) do
-    nums
-    |> Enum.with_index
-    
-    for x <- nums, y <- nums -- [x], z <- nums -- [x, y] do
-      case x + y + z == 0 do
-        true -> Result.add([x,y,z])
-        false -> nil
+    nums_with_index =
+      nums
+      |> Enum.with_index()
+
+    result =
+      for x <- nums_with_index,
+          y <- nums_with_index -- [x],
+          z <- nums_with_index -- [x, y],
+          reduce: [] do
+        acc ->
+          {v1, i1} = x
+          {v2, i2} = y
+          {v3, i3} = z
+
+          case v1 + v2 + v3 == 0 and i1 < i2 and i2 < i3 do
+            true ->
+              [[v1, v2, v3] | acc]
+
+            false ->
+              acc
+          end
       end
-    end
 
-    Result.get()
+    result
+    |> Enum.uniq_by(fn x -> Enum.sort(x) end)
   end
 end
 
+# defmodule Result do
+#   use Agent
 
-defmodule Result do
-  use Agent
-  def start_link() do
-    Agent.start_link(fn -> [] end, name: __MODULE__)
-  end
+#   def start_link() do
+#     Agent.start_link(fn -> [] end, name: __MODULE__)
+#   end
 
-  def add(e) do
-    Agent.update(__MODULE__, fn acc ->
-      [e | acc]
-    end)
-  end
+#   def add(e) do
+#     Agent.update(__MODULE__, fn acc ->
+#       [e | acc]
+#     end)
+#   end
 
-  def get() do
-    Agent.get(__MODULE__, fn acc -> acc end)
-  end
-end
+#   def get() do
+#     Agent.get(__MODULE__, fn acc -> acc end)
+#   end
+# end
--- leetcode/test/15/three_sum_test.exs
+++ leetcode/test/15/three_sum_test.exs
@@ -1,11 +1,13 @@
 defmodule ThreeSumTest do
   use ExUnit.Case, async: true
-  
+
   describe "baseline" do
     test "01" do
-      assert [[-1, -1, 2], [-1, 0, 1]] = ThreeSum.three_sum([-1,0,1,2,-1,-4])
+      assert [[0, 1, -1], [-1, 2, -1]] = ThreeSum.three_sum([-1, 0, 1, 2, -1, -4])
       assert [] = ThreeSum.three_sum([])
       assert [] = ThreeSum.three_sum([0])
+
+      assert [[0,0,0]] = ThreeSum.three_sum([0, 0, 0])
     end
   end
 end
